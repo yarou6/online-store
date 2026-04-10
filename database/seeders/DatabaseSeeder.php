6bf724af0ffefec1ad643c3bcb2822992d646aa9
user <1@mail.ru>
2026-04-10 14:44:23 +1000

    feat!: create auth and other controllers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Cart;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -17,14 +18,18 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-//        User::factory()->create([
-//            'name' => 'admin',
-//            'email' => 'admin@admin.com',
-//            'password' => 'admin',
-//            'role' => 'admin',
-//            'phone' => '0',
-//            'created_at' => now(),
-//            'updated_at' => now(),
-//        ]);
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => 'admin',
+            'role' => 'admin',
+            'phone' => '0',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+        Cart::factory()->create([
+            'user_id' => 1,
+            'session_id' => null,
+        ]);
     }
 }
