ff81675d374a2435e4716d467a5e725ca13176b2
user <1@mail.ru>
2026-04-10 12:58:56 +1000

    feat!: create models, factories, and migrations

---
+++ app/Models/Category.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Category extends Model
+{
+    /** @use HasFactory<\Database\Factories\CategoryFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'created_at',
+        'updated_at',
+    ];
+    public function category_products(): HasMany
+    {
+        return $this->hasMany(Category_product::class);
+    }
+}
---
+++ app/Models/Order_item.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order_item extends Model
+{
+    /** @use HasFactory<\Database\Factories\OrderItemFactory> */
+    use HasFactory;
+
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+    public function users(): HasMany
+    {
+        return $this->hasMany(User::class);
+    }
+}
